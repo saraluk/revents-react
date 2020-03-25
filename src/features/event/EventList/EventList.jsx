a14a37b2fe0852c2c4ee3199b9bfc9d1513494e3
Saraluk <saralukkai@gmail.com>
2020-03-25 16:46:25 -0400

    added state to EventDashboard

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -5,9 +5,9 @@ export default class EventList extends Component {
   render() {
     return (
       <Fragment>
-        <EventListItem />
-        <EventListItem />
-        <EventListItem />
+        {this.props.events.map(event => (
+          <EventListItem key={event.id} event={event} />
+        ))}
       </Fragment>
     );
   }
--- src/features/event/EventList/EventListAttendee.jsx
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { List, Image } from "semantic-ui-react";
 
 export default class EventListAttendee extends Component {
   render() {
-    return <div></div>;
+    const { attendee } = this.props;
+    return (
+      <List.Item>
+        <Image as='a' size='mini' circular src={attendee.photoURL} />
+      </List.Item>
+    );
   }
 }

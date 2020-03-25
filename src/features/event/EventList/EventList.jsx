aa6e8f1aeeec7c31530fbbf82ff8cdea5923bf77
Saraluk <saralukkai@gmail.com>
2020-03-24 23:22:24 -0400

    added event list

---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import EventListItem from "./EventListItem";
+
+export default class EventList extends Component {
+  render() {
+    return (
+      <Fragment>
+        <EventListItem />
+        <EventListItem />
+        <EventListItem />
+      </Fragment>
+    );
+  }
+}
---
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class EventListAttendee extends Component {
+  render() {
+    return <div></div>;
+  }
+}
